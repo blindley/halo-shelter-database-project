08f652312f757164ed19fdcb144f2b1fe35ed43c
Benjamin Lindley <benjameslindley@gmail.com>
2023-02-12 16:12:35 -0600

    moved data files to data directory

--- generate-tables.py
+++ generate-tables.py
@@ -6,9 +6,9 @@ def load_name_list(filename):
     with open(filename) as f:
         return [name.capitalize() for name in (line.strip() for line in f) if len(name) > 0]
 
-g_female_names = load_name_list('female-names.txt')
-g_male_names = load_name_list('male-names.txt')
-g_last_names = load_name_list('last-names.txt')
+g_female_names = load_name_list('data/female-names.txt')
+g_male_names = load_name_list('data/male-names.txt')
+g_last_names = load_name_list('data/last-names.txt')
 
 def random_entry(the_list):
     index = random.randint(0, len(the_list) - 1)
